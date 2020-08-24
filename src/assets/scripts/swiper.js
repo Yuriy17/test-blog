30efd8161ffa04dda6ff8e298f1bda20f7c85852
Yuriy <yuriy_sh@zoho.com>
2020-08-24 14:32:03 +0300

    fix style artcle, swiper

--- src/assets/scripts/swiper.js
+++ src/assets/scripts/swiper.js
@@ -1,15 +1,19 @@
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
+import { Swiper,Navigation } from 'swiper';
 import 'swiper/swiper-bundle.css';
+
+// configure Swiper to use modules
+Swiper.use([Navigation ]);
+// import Swiper styles
 
 export default new Swiper('.swiper-container', {
   // Optional parameters
+  observer: true,
+  observeParents: true,
   direction: 'horizontal',
   loop: true,
   slidesPerView: 3,
-  spaceBetween: 10,
-  loopedSlides:1,
+  spaceBetween: 28,
+  // loopedSlides:1,
 
   // If we need pagination
   pagination: {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './assets/stylesheets/index.scss';
 
-import  './assets/scripts/swiper';
+import  swiper from './assets/scripts/swiper';
 import select from './assets/scripts/select-lang';
 
 select();
+document.addEventListener('load',()=>{
+  swiper.navigation.update();
+})
